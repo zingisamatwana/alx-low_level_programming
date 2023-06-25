fe5bf90710cf4cef059c6cc9487ac823a645fe02
Zingisa Matwana <zingisamatwana@gmail.com>
2023-06-25 20:26:47 +0000

    Commit - Introduction to C Language project files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,13 @@
  *The code prints a string to stdout
  */
 #include <stdio.h>
-int main(void)
-{
-/*
- * Main print out a string to standard output using puts function
+
+/**
+ * main - This is missing short description
+ * Description
+ * for function Main
  */
+void main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
