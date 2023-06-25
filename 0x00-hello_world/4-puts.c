b5a7d695e763879cf2ad8ff42c098c45d83b4bc3
Zingisa Matwana <zingisamatwana@gmail.com>
2023-06-25 19:43:19 +0000

    Commit - Introduction to C Language project files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/*
+ *This file contains C source code
+ *The code prints a string to stdout
+ */
+int main(void)
+{
+	puts('\"Programming is like building a multilingual puzzle\n');
+	return (0);
+}
