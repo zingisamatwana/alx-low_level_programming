8cf591be4f79639ee6624f2a68d2eb8f7ce2396f
Zingisa Matwana <zingisamatwana@gmail.com>
2023-06-25 20:34:27 +0000

    Commit - Introduction to C Language project files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ *This file contains C source code
+ *The code prints a string to stdout
+ */
+#include <stdio.h>
+
+/**
+ * main - This is missing short description
+ * Description
+ * for function Main
+ * Return: 0 always
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c~
@@ -0,0 +1,17 @@
+/*
+ *This file contains C source code
+ *The code prints a string to stdout
+ */
+#include <stdio.h>
+
+/**
+ * main - This is missing short description
+ * Description
+ * for function Main
+ * Return: 0 always
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
