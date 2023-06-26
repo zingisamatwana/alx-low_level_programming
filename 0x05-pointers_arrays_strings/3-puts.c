3ff6c2f608b428d9f38d1e2a68bf2e2494581fe8
Zingisa Matwana <zingisamatwana@gmail.com>
2023-06-26 21:11:10 +0000

    Shell Variable Expansion

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+/*
+ *This file contains C source code
+ *The code prints a string to stdout
+ */
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - This is missing short description
+ * @str: define a char variable
+ *Return: string length
+ */
+int _puts(char *str)
+{
+	puts(str);
+}
