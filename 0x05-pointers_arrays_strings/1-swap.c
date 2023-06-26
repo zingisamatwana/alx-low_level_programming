59da7fdbeecbb32f73ce93138b9caf1852e496bc
Zingisa Matwana <zingisamatwana@gmail.com>
2023-06-26 20:45:46 +0000

    Shell Variable Expansion

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+/*
+ *This file contains C source code
+ *The code prints a string to stdout
+ */
+#include <stdio.h>
+
+/**
+ * swap_int - This is missing short description
+ * @a: define a pointer
+ * @b: define a pointer
+ * for function Main
+ * Return: 0 always
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
