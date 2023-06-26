05bf255fbe71e82964f60a1c1b9b40c01fac7b4d
Zingisa Matwana <zingisamatwana@gmail.com>
2023-06-26 21:43:26 +0000

    Shell Variable Expansion

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,8 +15,8 @@ void print_rev(char *s)
 	int length = strlen(s);
 	int i;
 
-	for (i = length; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 	{
-		printf("%c", s[i]);
+		printf("%c\n", s[i]);
 	}
 }
