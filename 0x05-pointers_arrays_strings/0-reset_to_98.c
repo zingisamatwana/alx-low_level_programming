96db4376a3c1ddc5519b2ba5384407412cb859eb
Zingisa Matwana <zingisamatwana@gmail.com>
2023-06-26 20:31:38 +0000

    Shell Variable Expansion

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,20 +11,3 @@ void reset_to_98(int *n)
 {
 	*n=98;
 }
-
-/**
- * main - This is missing short description
- * Description
- * for function Main
- * Return: 0 always
- */
-int main(void)
-{
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
-}
