2b460527cf356b1627050706737020d9135b0356
Zingisa Matwana <zingisamatwana@gmail.com>
2023-06-26 21:49:07 +0000

    Shell Variable Expansion

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be reversed
+ */
+void rev_string(char *s)
+{
+	int length = strlen(s);
+	int i, j;
+	char temp;
+
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
