94addfe8f1ab8ceeffa0e613562a51c875f960af
Zingisa Matwana <zingisamatwana@gmail.com>
2023-09-14 23:39:26 +0000

    Variadic Functions Project

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,12 @@
-#ifndef MY_FUNCTIONS_H
-#define MY_FUNCTIONS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
 
 int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
 
-#endif /* MY_FUNCTIONS_H */
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
