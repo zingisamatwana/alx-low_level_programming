fc5745e2b1c5cd6178f2fc91ddf0e32cdc5ff50e
Zingisa Matwana <zingisamatwana@gmail.com>
2023-09-18 21:33:15 +0000

    Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,8 @@ int _sqrt_recursion(int n);
 int _sqrt_helper(int n, int i);
 int is_prime_number(int n);
 int is_prime_helper(int n, int divisor);
+int is_palindrome(char *s);
+int check_palindrome(char *s, int start, int end);
+int string_length(char *s);
 
 #endif /* MAIN_H */
