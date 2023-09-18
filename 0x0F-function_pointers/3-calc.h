d5d5342962d655de96a81e92fee3543c9d1ae023
Zingisa Matwana <zingisamatwana@gmail.com>
2023-09-18 06:27:50 +0000

    Function Pointers

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -13,6 +13,11 @@ typedef struct op
 } op_t;
 
 int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 #endif /* CALC_H */
 y
