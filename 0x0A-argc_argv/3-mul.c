e51b72306d4e45a2e47a3d481a326982bc3f5452
Zingisa Matwana <zingisamatwana@gmail.com>
2023-09-19 17:56:42 +0000

    Recursion

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/**
  * main - Entry point
- * @argc: number of command line arguments
- * @argv: array of string containing the arguments
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the arguments
  *
  * Return: 0 on success, 1 on error
  */
-
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+
 	if (argc != 3)
 	{
-		printf("%s\n", "Error");
-
+		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		int p = atoi(argv[1]) * atoi(argv[2]);
 
-		printf("%d\n", p);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
 
-	}
+	printf("%d\n", result);
 
 	return (0);
 }
