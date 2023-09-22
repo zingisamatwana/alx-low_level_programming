55a55b99f9791996669343990439cc9d77326b54
Zingisa Matwana <zingisamatwana@gmail.com>
2023-09-22 01:29:11 +0000

    Recursion

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,29 +2,24 @@
 #include "lists.h"
 
 /**
- * print_list - Print all the elements of a list_t list.
- * @h: a Point to the head of the list.
+ * print_list - Prints all the elements of a list_t list.
+ * @h: A pointer to the head of the list.
  *
  * Return: The number of nodes in the list.
  */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
-	size-t node_count = 0;
+	size_t node_count = 0;
 
 	while (h != NULL)
 	{
 		/* If str is NULL, print "[0] (nil)" */
-		if (h->str  == NULL)
-		{
-			printf("[] (nil)\n");
-
-		}
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
 
-		node_country++;
+		node_count++;
 		h = h->next;
 	}
 
