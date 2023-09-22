27bcb6d89d396075fd40daa0af154be3494af5f1
Zingisa Matwana <zingisamatwana@gmail.com>
2023-09-22 02:07:01 +0000

    Recursion

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,5 +9,6 @@
 size_t print_list(const list_t *h);
 int _putchar(char c); /* Add the _putchar prototype */
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* LISTS_H */
