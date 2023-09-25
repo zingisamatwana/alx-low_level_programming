77457b62fce7be73b07ceef6b72febf9b0a9918a
Zingisa Matwana <zingisamatwana@gmail.com>
2023-09-25 10:10:34 +0000

    Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * before_main - Function to be executed before main.
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
